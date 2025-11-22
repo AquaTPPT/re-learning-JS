ee122cb41499607357cffe34e5ec0d99b7d35f99
AquaTPPT <195988559+AquaTPPT@users.noreply.github.com>
2025-11-22 09:34:52 +0000

    added a button.

--- top-exercise-examples/hello.js
+++ top-exercise-examples/hello.js
@@ -7,3 +7,13 @@ title2.textContent = "hello!!!"
 title2.classList.add("orange");
 content.appendChild(div);
 div.appendChild(title2);
+
+const but = document.createElement("button");
+but.setAttribute("id","helloWorld");
+but.textContent = "Click me!!";
+
+div.appendChild(but);
+
+
+const butCode = document.querySelector("#helloWorld");
+butCode.onclick = () => alert("hello World!");
